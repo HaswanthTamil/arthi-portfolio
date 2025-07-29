764b1fbc71c3009fbdbe1c46a147158eebc3d75f
ghost <ghost@devmail.com>
2025-07-29 16:26:00 +0530

    Change config to accomodate fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,29 +1,27 @@
-import Header from "@/components/layout/Header"
-import "@/styles/globals.css"
-import { Metadata } from "next"
-import { poppins, robotoFlex, lexend } from "@/lib/fonts"
+import "@/styles/globals.css";
+import { Metadata } from "next";
+import { poppins, robotoFlex, lexend } from "@/lib/fonts";
 
 export const metadata: Metadata = {
-  title: "Haswanth Tamil",
-  description: "Full Stack Developer",
+  title: "Arthi M",
+  description: "UI/UX Designer & Visual Creative",
   icons: {
     icon: "/favicon.png",
   },
-}
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
       <body
-        className={`relative ${poppins.variable} ${robotoFlex.variable} ${lexend.variable}`}
+        className={`${poppins.variable} ${robotoFlex.variable} ${lexend.variable}`}
       >
-        <Header />
         {children}
       </body>
     </html>
-  )
+  );
 }
